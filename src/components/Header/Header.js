0b58ed07873516751284e35361150502645466bf
Alex Taboada <alexandrotaboada@gmail.com>
2022-09-29 15:24:11 -0300

    header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,6 @@ import './Header.css'
 const Header = ({wallet}) => {
 
     const extractMiddle = (str) => {
-        if(!str) return
         const first = str.substr(0, 2);
         const second = str.substr(str.length - 5, str.length - 1)
         return first + "..." + second
@@ -12,7 +11,7 @@ const Header = ({wallet}) => {
 
     return(
         <header>
-            {extractMiddle(wallet)}
+            <p className='address'>{extractMiddle(wallet)}</p>
         </header>
     )
 }
